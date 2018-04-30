353aba782e10ae95264ef9ec834c366478688f55
dangry06 <duanyong@qq.com>
2018-04-30 15:59:12 +0800

    添加spring开头的工程

---
+++ spring-boot-rabbitmq/src/main/java/com/dangry/boot/rabbit/object/ObjectReceiver.java
@@ -0,0 +1,18 @@
+package com.dangry.boot.rabbit.object;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import com.dangry.boot.model.User;
+
+@Component
+@RabbitListener(queues = "object")
+public class ObjectReceiver {
+
+    @RabbitHandler
+    public void process(User user) {
+        System.out.println("Receiver object : " + user);
+    }
+
+}
